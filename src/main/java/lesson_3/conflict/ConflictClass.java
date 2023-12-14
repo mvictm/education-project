b5f21bd4bbfad0bc8d09f72ee0a9c43741a51472
Морозов Максим <maksim.morozov@gazprombank.ru>
2023-12-14 16:59:57 +0400

    1: Пример конфликта

--- src/main/java/lesson_3/conflict/ConflictClass.java
+++ src/main/java/lesson_3/conflict/ConflictClass.java
@@ -1,13 +1,13 @@
 package lesson_3.conflict;
 
 public class ConflictClass {
-    private static int number = 10;
+    private static int number = 154;
 
     public static void main(String[] args) {
         int count = 15;
 
-        System.out.println("Некий текст");
+        System.out.println("Обновленный некий текст");
         System.out.println(count * number);
-        System.out.println("Текст после");
+        System.out.println("Текст после с обновлением");
     }
 }
