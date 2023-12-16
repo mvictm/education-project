abb1124acecad0e6d72c7753ac8a2cf903f5921d
Морозов Максим <maksim.morozov@gazprombank.ru>
2023-12-16 23:45:19 +0400

    Занятие номер 3

---
+++ src/main/java/lesson_3/construction/ConditionalExpression.java
@@ -0,0 +1,16 @@
+package lesson_3.construction;
+
+public class ConditionalExpression {
+
+    public void ifElse() {
+
+    }
+
+    public void switchExpression(int first) {
+
+    }
+
+    public void ternary() {
+
+    }
+}
